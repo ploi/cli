986d76f83defdfdaeef79ba39caf0a2923cc424b
Stan Menten <stanmenten@live.nl>
2025-01-01 17:38:56 +0100

    Reworked SSH command

--- app/Commands/SshLoginCommand.php
+++ app/Commands/SshLoginCommand.php
@@ -2,18 +2,18 @@
 
 namespace App\Commands;
 
+use App\Commands\Concerns\InteractWithServer;
 use App\Traits\EnsureHasToken;
 use App\Traits\HasPloiConfiguration;
 use Illuminate\Support\Facades\Process;
 
-use function Laravel\Prompts\select;
 use function Laravel\Prompts\text;
 
 class SshLoginCommand extends Command
 {
-    use EnsureHasToken, HasPloiConfiguration;
+    use EnsureHasToken, HasPloiConfiguration, InteractWithServer;
 
-    protected $signature = 'ssh {--s|server=} {--u|user=}';
+    protected $signature = 'ssh {--server=} {--u|user=}';
 
     protected $description = 'Login to your server via SSH';
 
@@ -23,42 +23,9 @@ class SshLoginCommand extends Command
     {
         $this->ensureHasToken();
 
-        $server = $this->option('server');
-        $user = $this->option('user');
+        $serverId = $this->getServerId();
 
-        if (! $server) {
-            $servers = $this->ploi->getServerList()['data'];
-
-            $serverId = select(
-                'Select a server:',
-                collect($servers)
-                    ->mapWithKeys(fn ($server) => [
-                        $server['id'] => $server['name'].' ('.$server['ip_address'].')',
-                    ])
-                    ->toArray()
-            );
-
-            $this->server = $this->ploi->getServerDetails(serverId: $serverId)['data'];
-        } else {
-            $searchServers = $this->ploi->getServerList(search: $server)['data'];
-
-            if (count($searchServers) > 1) {
-                $this->error('Multiple servers found! Please specify the server id or server ip address.');
-                $serverId = select(
-                    'Select a server:',
-                    collect($searchServers)
-                        ->mapWithKeys(fn ($server) => [
-                            $server['id'] => $server['name'].' ('.$server['ip_address'].')',
-                        ])
-                        ->toArray()
-                );
-
-                $this->server = $this->ploi->getServerDetails(serverId: $serverId)['data'];
-                exit();
-            } else {
-                $this->server = $searchServers[0];
-            }
-        }
+        $this->server = $this->ploi->getServerDetails($serverId)['data'];
 
         $user = $user ?? text(label: 'Enter the username you want to login with', default: 'ploi');
 
